fbd1d05a6a565d49a27d04b574a9592cc430a644
majerusjl <44145662+majerusjl@users.noreply.github.com>
2021-02-24 16:39:10 -0600

    Create diskinfo.py

---
+++ diskinfo.py
@@ -0,0 +1,16 @@
+# Show hard drives under Windows 10 and identify which disk the OS is booted on.
+import subprocess, wmi
+
+def diskcount():
+   w = wmi.WMI()
+   x = 0
+   for drive in w.Win32_DiskDrive():
+        x+=1
+   return(x)
+
+num = diskcount()
+for i in range(num):
+   print(" ")
+   disks = subprocess.Popen(['powershell', 'get-disk {} | format-list model, isboot'.format(i)], stdout=subprocess.PIPE).communicate()[0]
+   print(disks.strip().decode('utf-8'))
+   print(" ")
